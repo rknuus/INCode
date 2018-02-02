f794b2ad5a726dafaa704d6ebe1fe77edbecc5d6
Raphael Knaus <rknuus@gmail.com>
2018-02-02 17:13:31 +0100

    determine callables from actual code

--- INCode/models.py
+++ INCode/models.py
@@ -1,8 +1,41 @@
-class File(object):
-    '''Represents a file and provides a list of callables'''
+from clang.cindex import CursorKind, Index, TranslationUnitLoadError
 
-    def __init__(self, entry_file, **kwargs):
+
+class File(object):
+    '''Represents a file and provides a list of callables.'''
+
+    def __init__(self, file, **kwargs):
         super(File, self).__init__()
+        self.index_ = Index.create()
+        try:
+            self.tu_ = self.index_.parse(file, **kwargs)
+        except TranslationUnitLoadError:
+            raise ValueError('Cannot parse file {}'.format(file))
 
     def get_callables(self):
-        return []
+        return self._get_function_definitions()
+
+    def _get_function_definitions(self):
+        filename = self.tu_.cursor.spelling
+        for cursor in self.tu_.cursor.walk_preorder():
+            if cursor.location.file is None or cursor.location.file.name != filename:
+                pass
+            elif cursor.kind == CursorKind.FUNCTION_DECL:
+                yield Callable(self._get_function_signature(cursor), cursor)
+
+    @staticmethod
+    def _get_function_signature(cursor):
+        return '{} {}'.format(cursor.result_type.spelling, cursor.displayname)
+
+
+class Callable(object):
+    '''Represents a Callable and provides a list of callables used by this callable.'''
+
+    def __init__(self, name, cursor):
+        super(Callable, self).__init__()
+        self.name_ = name
+        self.cursor_ = cursor
+
+    # TODO(KNR): replace by read-only attribute
+    def get_name(self):
+        return self.name_
--- tests/model_test.py
+++ tests/model_test.py
@@ -1,6 +1,14 @@
-from INCode.models import File
+from INCode.models import Callable, File
 
 
 def test__file__get_callables_for_empty_file__returns_empty_list():
     file = File('empty.cpp', unsaved_files=[('empty.cpp', '\n')])
-    assert file.get_callables() == []
+    assert list(file.get_callables()) == []
+
+
+def test__file__get_callables_for_file_with_one_function__returns_that_function():
+    file = File('one_function.cpp', unsaved_files=[('one_function.cpp', 'void a() {}\n')])
+    # TODO(KNR): how to ignore the second parameter when comparing Callables?
+    callables = list(file.get_callables())
+    assert len(callables) == 1
+    assert callables[0].get_name() == 'void a()'

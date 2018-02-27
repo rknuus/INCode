f2f091308ec280b188040ef0ae76050f533202ff
Raphael Knaus <rknuus@gmail.com>
2018-02-27 21:14:48 +0100

    add index to lookup cross-TU-references

--- INCode/models.py
+++ INCode/models.py
@@ -1,34 +1,54 @@
-from clang.cindex import CursorKind, Index, TranslationUnitLoadError
+from clang.cindex import CursorKind, TranslationUnitLoadError
+from clang import cindex
 
 
 def _get_function_signature(cursor):
     return '{} {}'.format(cursor.result_type.spelling, cursor.displayname)
 
 
+class Index(object):
+    def __init__(self):
+        super(Index, self).__init__()
+        self.index_ = cindex.Index.create()
+        self.table_ = {}
+
+    def register(self, cursor):
+        self.table_[cursor.get_usr()] = cursor
+
+    def lookup(self, usr):
+        return self.table_[usr]
+
+    # TODO(KNR): replace by read-only attribute
+    def get_clang_index(self):
+        return self.index_
+
+
 class File(object):
     '''Represents a file and provides a list of callables.'''
 
-    def __init__(self, file, **kwargs):
+    def __init__(self, file, index, **kwargs):
         super(File, self).__init__()
-        self.index_ = Index.create()
+        self.index_ = index
         try:
-            self.tu_ = self.index_.parse(file, **kwargs)
+            self.tu_ = self.index_.get_clang_index().parse(file, **kwargs)
         except TranslationUnitLoadError:
             raise ValueError('Cannot parse file {}'.format(file))
 
     def get_callables(self):
         for cursor in self.tu_.cursor.walk_preorder():
             if cursor.location.file is not None and cursor.kind == CursorKind.FUNCTION_DECL:
-                yield Callable(_get_function_signature(cursor), cursor)
+                yield Callable(_get_function_signature(cursor), cursor, self.index_)
 
 
 class Callable(object):
     '''Represents a Callable and provides a list of callables used by this callable.'''
 
-    def __init__(self, name, cursor):
+    def __init__(self, name, cursor, index):
         super(Callable, self).__init__()
         self.name_ = name
         self.cursor_ = cursor
+        self.index_ = index
+        self.index_.register(cursor)
 
     # TODO(KNR): replace by read-only attribute
     def get_name(self):
@@ -42,4 +62,5 @@ class Callable(object):
             if cursor.kind == CursorKind.CALL_EXPR:
                 # TODO(KNR): what's the point of `get_definition()` in list_references_of_func.py?
                 definition = cursor.referenced
-                yield Callable(_get_function_signature(definition), definition)  # TODO(KNR): pass self as parent
+                # TODO(KNR): pass self as parent
+                yield Callable(_get_function_signature(definition), definition, self.index_)
--- tests/model_test.py
+++ tests/model_test.py
@@ -1,13 +1,15 @@
-from INCode.models import Callable, File
+from INCode.models import Callable, File, Index
 
 
 def test__file__get_callables_for_empty_file__returns_empty_list():
-    file = File('empty.cpp', unsaved_files=[('empty.cpp', '\n')])
+    index = Index()
+    file = File('empty.cpp', index, unsaved_files=[('empty.cpp', '\n')])
     assert list(file.get_callables()) == []
 
 
 def test__file__get_callables_for_file_with_one_function__returns_that_function():
-    file = File('one_function.cpp', unsaved_files=[('one_function.cpp', 'void a() {}\n')])
+    index = Index()
+    file = File('one_function.cpp', index, unsaved_files=[('one_function.cpp', 'void a() {}\n')])
     # TODO(KNR): how to ignore the second parameter when comparing Callables?
     callables = list(file.get_callables())
     assert len(callables) == 1
@@ -15,7 +17,9 @@ def test__file__get_callables_for_file_with_one_function__returns_that_function(
 
 
 def test__file__get_callables_for_file_with_two_functions__returns_both_functions():
-    file = File('two_functions.cpp', unsaved_files=[('two_functions.cpp', 'void a() {}\nvoid b(const int i) {}\n')])
+    index = Index()
+    file = File('two_functions.cpp', index,
+                unsaved_files=[('two_functions.cpp', 'void a() {}\nvoid b(const int i) {}\n')])
     # TODO(KNR): how to ignore the second parameter when comparing Callables?
     callables = list(file.get_callables())
     assert len(callables) == 2
@@ -25,13 +29,16 @@ def test__file__get_callables_for_file_with_two_functions__returns_both_function
 
 
 def test__file__get_referenced_callables_for_empty_function__returns_empty_list():
-    file = File('non_referencing_function.cpp', unsaved_files=[('non_referencing_function.cpp', 'void a() {}\n')])
+    index = Index()
+    file = File('non_referencing_function.cpp', index,
+                unsaved_files=[('non_referencing_function.cpp', 'void a() {}\n')])
     callable = next(file.get_callables())
     assert list(callable.get_referenced_callables()) == []
 
 
 def test__file__get_referenced_callables_for_function_calling_another_one__returns_that_function():
-    file = File('referencing_function.cpp',
+    index = Index()
+    file = File('referencing_function.cpp', index,
                 unsaved_files=[('referencing_function.cpp', 'void a() {}\nvoid b() {\na();\n}\n')])
     callables = list(file.get_callables())
     callable = callables[1]
@@ -41,7 +48,8 @@ def test__file__get_referenced_callables_for_function_calling_another_one__retur
 
 
 def test__file__get_referenced_callables_for_function_calling_two_others__returns_both_function():
-    file = File('referencing_function.cpp',
+    index = Index()
+    file = File('referencing_function.cpp', index,
                 unsaved_files=[('referencing_function.cpp', 'void a() {}\nvoid b() {}\nvoid c() {\na();\nb();\n}\n')])
     callables = list(file.get_callables())
     callable = callables[2]
@@ -52,14 +60,16 @@ def test__file__get_referenced_callables_for_function_calling_two_others__return
 
 
 def test__file__get_referenced_callables_for_referenced_function_in_another_file__returns_that_function():
+    index = Index()
     # TODO(KNR): don't know how to use unsaved_files for multiple files...
     # file = File(
-    #     'cross_tu_referencing_function.cpp',
+    #     'cross_tu_referencing_function.cpp', index,
     #     args=['-I./'],
     #     unsaved_files=[('cross_tu_referencing_function.cpp', '#include "dependency.h"\nvoid b() {\na();\n}\n'), (
     #         'dependency.h', '#pragma once\nvoid a();\n'), ('dependency.cpp', '#include "dependency.h"\nvoid a() {}\n')
     #                    ])
-    file = File('trials/cross_tu_referencing_function.cpp', args=['-I./trials'])
+    # TODO(KNR): generate bloody source files in a temporary directory
+    file = File('trials/cross_tu_referencing_function.cpp', index, args=['-I./trials'])
     callables = list(file.get_callables())
     # callable = callables[0]
     callable = callables[1]
@@ -69,8 +79,9 @@ def test__file__get_referenced_callables_for_referenced_function_in_another_file
 
 
 def test__file__identify_definition_for_referenced_function_in_same_file__returns_that_definition():
+    index = Index()
     file = File(
-        'identify_local_function.cpp',
+        'identify_local_function.cpp', index,
         unsaved_files=[('identify_local_function.cpp', 'void a() {}\nvoid b() {}\nvoid c() {\na();\nb();\n}\n')])
     callables = list(file.get_callables())
     callable = callables[2]
@@ -81,8 +92,30 @@ def test__file__identify_definition_for_referenced_function_in_same_file__return
 
 
 def test__file__identify_definition_for_referenced_function_in_another_file__returns_that_definition():
-    file = File('trials/cross_tu_referencing_function.cpp', args=['-I./trials'])
+    index = Index()
+    file = File('trials/cross_tu_referencing_function.cpp', index, args=['-I./trials'])
     callables = list(file.get_callables())
     callable = callables[1]
     referenced_callables = list(callable.get_referenced_callables())
     assert referenced_callables[0].get_usr() == 'c:@F@a#'
+
+
+def test__file__get_callables_for_local_functions__registers_callables_in_index():
+    index = Index()
+    file = File(
+        'identify_local_function.cpp', index,
+        unsaved_files=[('identify_local_function.cpp', 'void a() {}\nvoid b() {}\nvoid c() {\na();\nb();\n}\n')])
+    list(file.get_callables())
+    index.lookup('c:@F@a#').location.file.name == 'identify_local_function.cpp'
+    index.lookup('c:@F@b#').location.file.name == 'identify_local_function.cpp'
+    index.lookup('c:@F@c#').location.file.name == 'identify_local_function.cpp'
+
+
+def test__file__get_callables_for_function_in_another_file__registers_callables_in_index():
+    index = Index()
+    dependency_file = File('trials/dependency.cpp', index, args=['-I./trials'])
+    list(dependency_file.get_callables())
+    file = File('trials/cross_tu_referencing_function.cpp', index, args=['-I./trials'])
+    list(file.get_callables())
+    index.lookup('c:@F@a#').location.file.name == './trials/dependency.cpp'
+    index.lookup('c:@F@b#').location.file.name == 'trials/cross_tu_referencing_function.cpp'

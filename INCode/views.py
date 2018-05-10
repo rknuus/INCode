6b8bd061952867f8ffaff3637526e3de4911a10f
Raphael Knaus <rknuus@gmail.com>
2018-05-10 17:38:12 +0200

    remove unused code

--- INCode/views.py
+++
@@ -1,9 +0,0 @@
-class EntryView(object):
-    '''View of the entry phase'''
-
-    def __init__(self):
-        super(EntryView, self).__init__()
-        self.callables_ = []
-
-    def load_callables(self, callables):
-        self.callables_ = callables

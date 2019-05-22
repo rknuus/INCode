1fa19803af5ab3a390173f8599c76741604fbf1f
Gian Furrer <gian.furrer@roche.com>
2019-05-22 16:05:55 +0200

    Optimize config

--- INCode/config.py
+++ INCode/config.py
@@ -1,21 +1,16 @@
-import os
-from enum import IntEnum
-import json
-from json import JSONDecodeError
 from configparser import ConfigParser
-
-FILE_NAME = "config.ini"
 
 
 class Config(ConfigParser):
     LOCAL_ONLY = "localonly"
 
-    def __init__(self):
+    def __init__(self, file_name="config.ini"):
         super().__init__()
-        self.read(FILE_NAME)
+        self.file_name_ = file_name
+        self.read(self.file_name_)
 
     def save(self):
-        with open(FILE_NAME, 'w') as config_file:
+        with open(self.file_name_, 'w') as config_file:
             self.write(config_file)
 
     def store(self, option, value):
@@ -25,7 +20,6 @@ class Config(ConfigParser):
     def load(self, option):
         if not self.has_option(self.default_section, option):
             return None
-        data = None
         try:
             data = self.getboolean(self.default_section, option)
         except Exception:

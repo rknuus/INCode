d303dabdaa710b18f54fa80a5bbdbed4ad649014
Raphael Knaus <rknuus@gmail.com>
2020-05-19 21:29:44 +0200

    Add TU access service for clang

--- INCode/clang_access.py
+++ INCode/clang_access.py
@@ -2,6 +2,7 @@
 
 from clang.cindex import CursorKind, Index
 from os import path
+import json
 
 
 clang_severity_str = {
@@ -20,7 +21,7 @@ def get_diagnostic_message(diag):
 
 
 class ClangCalltreeAccess(object):
-    '''Constructs the data model.'''
+    '''Constructs a call tree from given TUs.'''
     def __init__(self):
         super(ClangCalltreeAccess, self).__init__()
         self.call_tree_ = []
@@ -48,3 +49,22 @@ class ClangCalltreeAccess(object):
             self.call_tree_.append(node.displayname)
         for child in node.get_children():
             self.parse_(child)
+
+
+class ClangTUAccess(object):
+    '''Returns files and their compiler arguments from a compilation database.'''
+    def __init__(self, filename, extra_arguments=None):
+        super(ClangTUAccess, self).__init__()
+        self.extra_arguments_ = extra_arguments or ''
+        self.files_ = self.collect_files_(filename)
+
+    @property
+    def files(self):
+        return self.files_
+
+    def collect_files_(self, filename):
+        if not filename.endswith('compile_commands.json'):
+            return {filename: self.extra_arguments_}
+        with open(filename) as compdb:
+            db = json.load(compdb)
+        return {entry['file']: entry['command'] for entry in db}
--- tests/clang_calltree_access_test.py
+++ tests/clang_calltree_access_test.py
@@ -1,42 +1,42 @@
 # Copyright (C) 2020 R. Knuus
 
 
-from INCode.clang_calltree_access import ClangCalltreeAccess
-from tests.test_environment_generation import generate_tu
+from INCode.clang_access import ClangCalltreeAccess
+from tests.test_environment_generation import generate_file
 import pytest
 
 
 def test__given_one_function__parse_tree_with_one_element():
-    engine = ClangCalltreeAccess()
-    with generate_tu('one-function.cpp', 'void f() {}') as file_name:
-        engine.parse_tu(tu=file_name, compiler_arguments='')
-    assert len(engine.call_tree) == 1
+    access = ClangCalltreeAccess()
+    with generate_file('one-function.cpp', 'void f() {}') as file_name:
+        access.parse_tu(tu=file_name, compiler_arguments='')
+    assert len(access.call_tree) == 1
 
 
 def test__given_two_functions__parse_tree_with_two_elements():
-    engine = ClangCalltreeAccess()
-    with generate_tu('two-functions.cpp', 'void f() {}\nvoid g() {}') as file_name:
-        engine.parse_tu(tu=file_name, compiler_arguments='')
-    assert len(engine.call_tree) == 2
+    access = ClangCalltreeAccess()
+    with generate_file('two-functions.cpp', 'void f() {}\nvoid g() {}') as file_name:
+        access.parse_tu(tu=file_name, compiler_arguments='')
+    assert len(access.call_tree) == 2
 
 
 def test__given_a_function__parse_tree_containts_function_name():
-    engine = ClangCalltreeAccess()
-    with generate_tu('two-functions.cpp', 'void f() {}') as file_name:
-        engine.parse_tu(tu=file_name, compiler_arguments='')
+    access = ClangCalltreeAccess()
+    with generate_file('two-functions.cpp', 'void f() {}') as file_name:
+        access.parse_tu(tu=file_name, compiler_arguments='')
     expected = ['f()']
-    actual = engine.call_tree
+    actual = access.call_tree
     assert expected == actual
 
 
 def test__given_non_existing_file__parse_tree_throws():
-    engine = ClangCalltreeAccess()
+    access = ClangCalltreeAccess()
     with pytest.raises(OSError):
-        engine.parse_tu(tu='a-file-that-doesnt-exist', compiler_arguments='')
+        access.parse_tu(tu='a-file-that-doesnt-exist', compiler_arguments='')
 
 
 def test__given_file_with_syntax_error__parse_tree_throws():
-    engine = ClangCalltreeAccess()
-    with generate_tu('syntax-error.cpp', 'void f() {') as file_name:
+    access = ClangCalltreeAccess()
+    with generate_file('syntax-error.cpp', 'void f() {') as file_name:
         with pytest.raises(SyntaxError):
-            engine.parse_tu(tu=file_name, compiler_arguments='')
+            access.parse_tu(tu=file_name, compiler_arguments='')

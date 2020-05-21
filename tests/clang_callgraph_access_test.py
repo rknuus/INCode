5c17ed026d785173df2b7eb604a9fb01ade4cc5b
Raphael Knaus <rknuus@gmail.com>
2020-05-21 16:53:51 +0200

    Adjust test to changed error handling of unknown entry point

--- tests/clang_callgraph_access_test.py
+++ tests/clang_callgraph_access_test.py
@@ -15,6 +15,15 @@ def test__given_function_without_calls__get_empty_calls_list():
     assert expected == actual
 
 
+def test_given_unknown_entry_point__get_empty_calls_list():
+    access = ClangCallGraphAccess()
+    with generate_file('one-function.cpp', 'void f() {}') as file_name:
+        access.parse_tu(tu_file_name=file_name, compiler_arguments='')
+    expected = []
+    actual = access.get_calls_of('g()')
+    assert expected == actual
+
+
 def test__given_function_calling_another__parse_tu_contains_call():
     access = ClangCallGraphAccess()
     with generate_file('two-functions.cpp', 'void f() {}\nvoid g() {f();}') as file_name:
@@ -44,11 +53,3 @@ def test__given_file_with_syntax_error__parse_tu_throws():
     with generate_file('syntax-error.cpp', 'void f() {') as file_name:
         with pytest.raises(SyntaxError):
             access.parse_tu(tu_file_name=file_name, compiler_arguments='')
-
-
-def test_given_unknown_entry_point__dump_throws():
-    access = ClangCallGraphAccess()
-    with generate_file('one-function.cpp', 'void f() {}') as file_name:
-        access.parse_tu(tu_file_name=file_name, compiler_arguments='')
-    with pytest.raises(KeyError):
-        access.get_calls_of('g()')
